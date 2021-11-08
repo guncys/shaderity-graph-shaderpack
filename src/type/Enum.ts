c935df67161cac94cfd0e09261ce2b5c0ec4fe8c
KaYaF <d.guitar.c@gmail.com>
2021-11-08 15:57:55 +0900

    feat: add method to convert to enum values

--- src/type/Enum.ts
+++ src/type/Enum.ts
@@ -1,14 +1,26 @@
-export const AvailableShaderStage = {
+export const _AvailableShaderStage = {
   Vertex: 'vertex',
   Fragment: 'fragment',
   VertexAndFragment: 'vertexandfragment',
   Unknown: 'unknown',
 } as const;
 
-export type AvailableShaderStageEnum =
-  typeof AvailableShaderStage[keyof typeof AvailableShaderStage];
+export const AvailableShaderStage = {
+  ..._AvailableShaderStage,
+  fromString: (str: string) => {
+    for (const shaderStage of Object.values(_AvailableShaderStage)) {
+      if (shaderStage.toLowerCase() === str.toLowerCase()) {
+        return shaderStage as AvailableShaderStageEnum;
+      }
+    }
+    return AvailableShaderStage.Unknown;
+  },
+} as const;
 
-export const GUIMode = {
+export type AvailableShaderStageEnum =
+  typeof AvailableShaderStage[keyof typeof _AvailableShaderStage];
+
+export const _GUIMode = {
   Standard: 'standard',
   PullDown: 'pulldown',
   SetVector: 'setvector',
@@ -17,4 +29,16 @@ export const GUIMode = {
   Unknown: 'unknown',
 } as const;
 
-export type GUIModeEnum = typeof GUIMode[keyof typeof GUIMode];
+export const GUIMode = {
+  ..._GUIMode,
+  fromString: (str: string) => {
+    for (const mode of Object.values(_GUIMode)) {
+      if (mode.toLowerCase() === str.toLowerCase()) {
+        return mode as GUIModeEnum;
+      }
+    }
+    return GUIMode.Unknown;
+  },
+} as const;
+
+export type GUIModeEnum = typeof GUIMode[keyof typeof _GUIMode];
