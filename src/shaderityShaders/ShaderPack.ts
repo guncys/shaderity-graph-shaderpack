0b7a8876e397816ef533a93e9a75cde7d9bcf430
KaYaF <d.guitar.c@gmail.com>
2021-11-15 19:29:01 +0900

    feat: add glsl file of shader output node

--- src/shaderityShaders/ShaderPack.ts
+++ src/shaderityShaders/ShaderPack.ts
@@ -1,10 +1,5 @@
 import {ShaderNodeData} from './../type/Type';
-import sampleStandard from './samples/sampleStandard.vs.glsl';
-import sampleShaderOutput from './samples/sampleShaderOutput.vs.glsl';
-import samplePullDown from './samples/samplePullDown.fs.glsl';
+import fragmentColor from './fragment/fragmentColor.fs.glsl';
+import vertexPosition from './vertex/vertexPosition.vs.glsl';
 
-export const ShaderPack: ShaderNodeData[] = [
-  sampleStandard,
-  sampleShaderOutput,
-  samplePullDown,
-];
+export const ShaderPack: ShaderNodeData[] = [fragmentColor, vertexPosition];
---
+++ src/shaderityShaders/fragment/fragmentColor.fs.glsl
@@ -0,0 +1,11 @@
+// <SGSP> NodeName: fragment color
+// <SGSP> AvailableShaderStage: Fragment
+// <SGSP> GUIMode: Standard
+
+precision highp float;
+
+// <SGSP> SocketName: inVec4 color
+// <SGSP> ShaderOutputSocket: outVec4
+void fragmentColor(in vec4 inVec4, out vec4 outVec4) {
+  outVec4 = inVec4;
+}
