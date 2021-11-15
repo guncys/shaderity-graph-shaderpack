91ce7c29e1cbeb3e8cd12c3bd8e548929e60b68f
KaYaF <d.guitar.c@gmail.com>
2021-11-15 19:31:08 +0900

    feat: add sample of set vector node

--- src/shaderityShaders/ShaderPack.ts
+++ src/shaderityShaders/ShaderPack.ts
@@ -1,5 +1,10 @@
 import {ShaderNodeData} from './../type/Type';
+import uniformVector4 from './common/uniformVector4.glsl';
 import fragmentColor from './fragment/fragmentColor.fs.glsl';
 import vertexPosition from './vertex/vertexPosition.vs.glsl';
 
-export const ShaderPack: ShaderNodeData[] = [fragmentColor, vertexPosition];
+export const ShaderPack: ShaderNodeData[] = [
+  uniformVector4,
+  fragmentColor,
+  vertexPosition,
+];
---
+++ src/shaderityShaders/common/uniformVector4.glsl
@@ -0,0 +1,7 @@
+// <SGSP> NodeName: Vector4
+// <SGSP> GUIMode: SetVector
+
+// <SGSP> SocketName: outVec4 vector4
+void uniformVector4(in vec4 u_inVec4, out vec4 outVec4) {
+  outVec4 = u_inVec4;
+}
