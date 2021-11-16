c87ece4d3e9c0c88ff0293c3068c0f2577bcf90d
KaYaF <d.guitar.c@gmail.com>
2021-11-16 15:45:26 +0900

    chore: export some types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import {GUIMode, GUIModeEnum} from './type/Enum';
-import {ShaderNodeData} from './type/Type';
+import {
+  GUIMode,
+  GUIModeEnum,
+  AvailableShaderStage,
+  AvailableShaderStageEnum,
+} from './type/Enum';
+import {ShaderNodeData, PullDownItem} from './type/Type';
 import {ShaderPack} from './shaderityShaders/ShaderPack';
 
 export default {
   GUIMode,
   ShaderPack,
+  AvailableShaderStage,
 };
 
-export type {GUIModeEnum, ShaderNodeData};
+export type {
+  GUIModeEnum,
+  AvailableShaderStageEnum,
+  ShaderNodeData,
+  PullDownItem,
+};
