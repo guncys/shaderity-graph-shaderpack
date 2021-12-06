8415f70d2f0d1e4b56f01569199f9e624bba265c
KaYaF <d.guitar.c@gmail.com>
2021-12-06 15:56:05 +0900

    feat: add uv coodinate node

--- src/shaderityShaders/ShaderPack.ts
+++ src/shaderityShaders/ShaderPack.ts
@@ -1,16 +1,20 @@
 import {ShaderNodeData} from './../type/Type';
 import uniformVector4 from './common/uniformVector4.glsl';
-import fragmentColor from './fragment/fragmentColor.fs.glsl';
-import inputPosition from './vertex/inputPosition.vs.glsl';
-import vertexPosition from './vertex/vertexPosition.vs.glsl';
 import uniformColor from './common/uniformColor.glsl';
 import uniformTexture2D from './common/uniformTexture2D.glsl';
 
+import inputPosition from './vertex/inputPosition.vs.glsl';
+import vertexPosition from './vertex/vertexPosition.vs.glsl';
+import texcoord from './vertex/texcoord.glsl';
+
+import fragmentColor from './fragment/fragmentColor.fs.glsl';
+
 export const ShaderPack: ShaderNodeData[] = [
   uniformVector4,
-  fragmentColor,
-  inputPosition,
-  vertexPosition,
   uniformColor,
   uniformTexture2D,
+  inputPosition,
+  vertexPosition,
+  texcoord,
+  fragmentColor,
 ];
---
+++ src/shaderityShaders/vertex/texcoord.glsl
@@ -0,0 +1,23 @@
+// <SGSP> NodeName: UV Coordinate
+// <SGSP> AvailableShaderStage: Vertex
+// <SGSP> GUIMode: PullDown
+
+// <SGSP> PullDown_Description: Index
+
+// <SGSP> PullDown_DisplayName: 0
+void texcoord_0(
+  in vec2 a_texcoord_0,
+  in vec2 a_texcoord_1,
+  out vec2 uv
+) {
+  uv = a_texcoord_0;
+}
+
+// <SGSP> PullDown_DisplayName: 1
+void texcoord_1(
+  in vec2 a_texcoord_0,
+  in vec2 a_texcoord_1,
+  out vec2 uv
+) {
+  uv = a_texcoord_1;
+}
