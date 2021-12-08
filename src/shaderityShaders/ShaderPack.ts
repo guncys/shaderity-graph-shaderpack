9a435243e6d22385864520921f6cc4e0d117adae
KaYaF <d.guitar.c@gmail.com>
2021-12-08 20:17:53 +0900

    feat: add uniformTexture2D node (sampler input node)

--- src/shaderityShaders/ShaderPack.ts
+++ src/shaderityShaders/ShaderPack.ts
@@ -1,7 +1,8 @@
+import uniformTexture2D from './common/uniformTexture2D';
+
 import {AbstractNodeData} from './../type/Type';
 import uniformVector4 from './common/uniformVector4.glsl';
 import uniformColor from './common/uniformColor.glsl';
-import uniformTexture2D from './common/uniformTexture2D.glsl';
 import sampleTexture2D from './common/sampleTexture2D.glsl';
 
 import inputPosition from './vertex/inputPosition.vs.glsl';
@@ -11,9 +12,9 @@ import texcoord from './vertex/texcoord.glsl';
 import fragmentColor from './fragment/fragmentColor.fs.glsl';
 
 export const ShaderPack: AbstractNodeData[] = [
+  uniformTexture2D,
   uniformVector4,
   uniformColor,
-  uniformTexture2D,
   sampleTexture2D,
   inputPosition,
   vertexPosition,
