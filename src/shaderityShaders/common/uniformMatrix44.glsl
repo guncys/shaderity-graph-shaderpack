d7ea3df51dc4d63465c6393bbf37fef14b3d5343
KaYaF <d.guitar.c@gmail.com>
2021-12-16 18:36:09 +0900

    feat: remove useless parameter

--- src/shaderityShaders/common/uniformMatrix44.glsl
+++ src/shaderityShaders/common/uniformMatrix44.glsl
@@ -3,7 +3,6 @@
 // <SGSP> GUIMode: SetMatrix
 
 // <SGSP> SocketName: outMat4 mat44
-// <SGSP> SetMatrix_DefaultValues: 1.0 0 0 0   0 1.0 0 0   0 0 1.0 0   0 0 0 1.0
 void uniformMatrix44(in mat4 u_inMat4, out mat4 outMat4) {
   outMat4 = u_inMat4;
 }
